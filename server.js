b8c33b8cb1ef1068588c22bcf1edaa46478df4f8
Stéphan <rabemananjarafalimananastephan@gmail.com>
2024-06-26 12:07:21 +0300

    thrird

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', router);
+app.get('/', (req, res, next) => {
+    res.send("hello world");
+    next();
+})
 app.listen(config.port, config.host, () => {
     console.log(`Server is running on port http://${config.host}:${config.port}`);
 });
